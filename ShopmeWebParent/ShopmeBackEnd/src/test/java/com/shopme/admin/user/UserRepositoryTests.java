aa71f3de30807d413f5ec56d6d4382bfd0c23119
Nam Ha Minh <Nam Ha Minh@devpc>
2021-01-29 01:00:31 +0700

    Check uniqueness of user email

--- ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
@@ -90,4 +90,12 @@ public class UserRepositoryTests {
 		repo.deleteById(userId);
 		
 	}
+	
+	@Test
+	public void testGetUserByEmail() {
+		String email = "ravi@gmail.com";
+		User user = repo.getUserByEmail(email);
+		
+		assertThat(user).isNotNull();
+	}
 }
