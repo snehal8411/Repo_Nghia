6e41fa7e6753b29d2e5132e26cc981a937b55e49
Nam Ha Minh <Nam Ha Minh@devpc>
2021-02-27 14:36:26 +0700

    Code Refactor #1

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -103,7 +103,12 @@ public class UserController {
 		
 		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
 		
-		return "redirect:/users";
+		return getRedirectURLtoAffectedUser(user);
+	}
+
+	private String getRedirectURLtoAffectedUser(User user) {
+		String firstPartOfEmail = user.getEmail().split("@")[0];
+		return "redirect:/users/page/1?sortField=id&sortDir=asc&keyword=" + firstPartOfEmail;
 	}
 	
 	@GetMapping("/users/edit/{id}")
