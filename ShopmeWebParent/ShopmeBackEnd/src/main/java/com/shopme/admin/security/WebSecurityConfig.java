ccf19c520b365db222c47a8dce848da5cc6340a4
Nam Ha Minh <Nam Ha Minh@devpc>
2021-03-05 15:00:57 +0700

    Show user's full name and Code logout

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -49,7 +49,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.formLogin()			
 				.loginPage("/login")
 				.usernameParameter("email")
-				.permitAll();
+				.permitAll()
+			.and().logout().permitAll();
 	}
 
 	@Override
