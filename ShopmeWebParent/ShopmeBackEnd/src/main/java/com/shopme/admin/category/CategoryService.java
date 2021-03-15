8df328562a094ae1bd7faf419c0338724228f49a
Nam Ha Minh <Nam Ha Minh@devpc>
2021-03-15 23:42:21 +0700

    Code category form

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
@@ -1,6 +1,8 @@
 package com.shopme.admin.category;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,4 +17,44 @@ public class CategoryService {
 	public List<Category> listAll() {
 		return (List<Category>) repo.findAll();
 	}
+	
+	public List<Category> listCategoriesUsedInForm() {
+		List<Category> categoriesUsedInForm = new ArrayList<>();
+		
+		Iterable<Category> categoriesInDB = repo.findAll();
+		
+		for (Category category : categoriesInDB) {
+			if (category.getParent() == null) {
+				categoriesUsedInForm.add(new Category(category.getName()));
+				
+				Set<Category> children = category.getChildren();
+				
+				for (Category subCategory : children) {
+					String name = "--" + subCategory.getName();
+					categoriesUsedInForm.add(new Category(name));
+					
+					listChildren(categoriesUsedInForm, subCategory, 1);
+				}
+			}
+		}		
+		
+		return categoriesUsedInForm;
+	}
+	
+	private void listChildren(List<Category> categoriesUsedInForm, Category parent, int subLevel) {
+		int newSubLevel = subLevel + 1;
+		Set<Category> children = parent.getChildren();
+		
+		for (Category subCategory : children) {
+			String name = "";
+			for (int i = 0; i < newSubLevel; i++) {				
+				name += "--";
+			}
+			name += subCategory.getName();
+			
+			categoriesUsedInForm.add(new Category(name));
+			
+			listChildren(categoriesUsedInForm, subCategory, newSubLevel);
+		}		
+	}	
 }
