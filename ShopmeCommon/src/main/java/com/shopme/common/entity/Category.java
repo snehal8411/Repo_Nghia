dcf51c02177954d04d80d0bb2deee063cd4667f3
Nam Ha Minh <Nam Ha Minh@devpc>
2021-03-16 22:38:15 +0700

    Code update category function

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
@@ -149,6 +149,8 @@ public class Category {
 	
 	@Transient
 	public String getImagePath() {
+		if (this.id == null) return "/images/image-thumbnail.png";
+		
 		return "/category-images/" + this.id + "/" + this.image;
 	}
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
@@ -2,6 +2,7 @@ package com.shopme.admin.category;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -101,4 +102,12 @@ public class CategoryService {
 			listSubCategoriesUsedInForm(categoriesUsedInForm, subCategory, newSubLevel);
 		}		
 	}	
+	
+	public Category get(Integer id) throws CategoryNotFoundException {
+		try {
+			return repo.findById(id).get();
+		} catch (NoSuchElementException ex) {
+			throw new CategoryNotFoundException("Could not find any category with ID " + id);
+		}
+	}
 }
