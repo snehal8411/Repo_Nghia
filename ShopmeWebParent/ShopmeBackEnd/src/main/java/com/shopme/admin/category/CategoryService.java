fb2be4393e84e17badfdfbb9268d4f4413df5276
Nam Ha Minh <Nam Ha Minh@devpc>
2021-03-19 23:23:22 +0700

    Code filter function for categories list

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
@@ -22,12 +22,13 @@ import com.shopme.common.entity.Category;
 @Service
 @Transactional
 public class CategoryService {
-	private static final int ROOT_CATEGORIES_PER_PAGE = 4;
+	public static final int ROOT_CATEGORIES_PER_PAGE = 4;
 	
 	@Autowired
 	private CategoryRepository repo;
 	
-	public List<Category> listByPage(CategoryPageInfo pageInfo, int pageNum, String sortDir) {
+	public List<Category> listByPage(CategoryPageInfo pageInfo, int pageNum, String sortDir,
+			String keyword) {
 		Sort sort = Sort.by("name");
 		
 		if (sortDir.equals("asc")) {
@@ -38,13 +39,30 @@ public class CategoryService {
 		
 		Pageable pageable = PageRequest.of(pageNum - 1, ROOT_CATEGORIES_PER_PAGE, sort);
 		
-		Page<Category> pageCategories = repo.findRootCategories(pageable);
+		Page<Category> pageCategories = null;
+		
+		if (keyword != null && !keyword.isEmpty()) {
+			pageCategories = repo.search(keyword, pageable);	
+		} else {
+			pageCategories = repo.findRootCategories(pageable);
+		}
+		
 		List<Category> rootCategories = pageCategories.getContent();
 		
 		pageInfo.setTotalElements(pageCategories.getTotalElements());
 		pageInfo.setTotalPages(pageCategories.getTotalPages());
 		
-		return listHierarchicalCategories(rootCategories, sortDir);
+		if (keyword != null && !keyword.isEmpty()) {
+			List<Category> searchResult = pageCategories.getContent();
+			for (Category category : searchResult) {
+				category.setHasChildren(category.getChildren().size() > 0);
+			}
+			
+			return searchResult;
+			
+		} else {
+			return listHierarchicalCategories(rootCategories, sortDir);
+		}
 	}
 	
 	private List<Category> listHierarchicalCategories(List<Category> rootCategories, String sortDir) {
