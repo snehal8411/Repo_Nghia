15400425c1fbacd6914262700276aff5ff83ea0d
Nam Ha Minh <Nam Ha Minh@devpc>
2021-03-23 11:10:07 +0700

    Code brand form

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Brand.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Brand.java
@@ -12,6 +12,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name = "brands")
@@ -80,4 +81,10 @@ public class Brand {
 		return "Brand [id=" + id + ", name=" + name + ", categories=" + categories + "]";
 	}
 
+	@Transient
+	public String getLogoPath() {
+		if (this.id == null) return "/images/image-thumbnail.png";
+		
+		return "/brand-logos/" + this.id + "/" + this.logo;		
+	}
 }
