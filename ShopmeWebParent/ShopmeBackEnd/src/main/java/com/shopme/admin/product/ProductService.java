8c9df37c228740d3cd61cc74c641d32dca3638aa
Nam Ha Minh <Nam Ha Minh@devpc>
2021-03-28 16:04:37 +0700

    Code save product function

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductService.java
@@ -1,5 +1,6 @@
 package com.shopme.admin.product;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,5 +15,22 @@ public class ProductService {
 	
 	public List<Product> listAll() {
 		return (List<Product>) repo.findAll();
+	}
+	
+	public Product save(Product product) {
+		if (product.getId() == null) {
+			product.setCreatedTime(new Date());
+		}
+		
+		if (product.getAlias() == null || product.getAlias().isEmpty()) {
+			String defaultAlias = product.getName().replaceAll(" ", "-");
+			product.setAlias(defaultAlias);
+		} else {
+			product.setAlias(product.getAlias().replaceAll(" ", "-"));
+		}
+		
+		product.setUpdatedTime(new Date());
+		
+		return repo.save(product);
 	}
 }
