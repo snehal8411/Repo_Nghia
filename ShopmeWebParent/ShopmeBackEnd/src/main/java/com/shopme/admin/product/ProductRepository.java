c5b7961ef904ab91795c66d7907f362d7fa21b73
Nam Ha Minh <Nam Ha Minh@devpc>
2021-03-29 17:14:14 +0700

    Code check uniquess of product

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import com.shopme.common.entity.Product;
 
 public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
-
+	
+	public Product findByName(String name);
 }
