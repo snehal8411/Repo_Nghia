999c6a05b84c30f92be66a78beda9a9720194660
Nam Ha Minh <Nam Ha Minh@devpc>
2021-03-30 14:58:02 +0700

    Code upload product images: entity classes

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Product.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Product.java
@@ -1,7 +1,10 @@
 package com.shopme.common.entity;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -53,6 +57,9 @@ public class Product {
 	private float height;
 	private float weight;
 	
+	@Column(name = "main_image", nullable = false)
+	private String mainImage;
+		
 	@ManyToOne
 	@JoinColumn(name = "category_id")
 	private Category category;
@@ -60,6 +67,9 @@ public class Product {
 	@ManyToOne
 	@JoinColumn(name = "brand_id")	
 	private Brand brand;
+	
+	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+	private Set<ProductImage> images = new HashSet<>();
 
 	public Integer getId() {
 		return id;
@@ -209,6 +219,24 @@ public class Product {
 	public String toString() {
 		return "Product [id=" + id + ", name=" + name + "]";
 	}
+
+	public String getMainImage() {
+		return mainImage;
+	}
+
+	public void setMainImage(String mainImage) {
+		this.mainImage = mainImage;
+	}
+
+	public Set<ProductImage> getImages() {
+		return images;
+	}
+
+	public void setImages(Set<ProductImage> images) {
+		this.images = images;
+	}
 	
-		
+	public void addExtraImage(String imageName) {
+		this.images.add(new ProductImage(imageName, this));
+	}
 }
