e2e3ff7a911f79fb7d8ac600d1250989dbfcf087
Nam Ha Minh <Nam Ha Minh@devpc>
2021-04-12 16:13:59 +0700

    Code product details page

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
@@ -11,6 +11,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.OrderBy;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -40,6 +41,7 @@ public class Category {
 	private Category parent;
 	
 	@OneToMany(mappedBy = "parent")
+	@OrderBy("name asc")
 	private Set<Category> children = new HashSet<>();
 
 	public Category() {
--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/product/ProductService.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/product/ProductService.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.shopme.common.entity.Product;
+import com.shopme.common.exception.ProductNotFoundException;
 
 @Service
 public class ProductService {
@@ -19,6 +20,14 @@ public class ProductService {
 		Pageable pageable = PageRequest.of(pageNum - 1, PRODUCTS_PER_PAGE);
 		
 		return repo.listByCategory(categoryId, categoryIdMatch, pageable);
+	}
+	
+	public Product getProduct(String alias) throws ProductNotFoundException {
+		Product product = repo.findByAlias(alias);
+		if (product == null) {
+			throw new ProductNotFoundException("Could not find any product with alias " + alias);
+		}
 		
+		return product;
 	}
 }
