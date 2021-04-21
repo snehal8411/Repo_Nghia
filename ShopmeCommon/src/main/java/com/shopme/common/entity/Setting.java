028456506dbd372648c7e28fabf39be30d31ae25
Nam Ha Minh <Nam Ha Minh@devpc>
2021-04-21 19:51:45 +0700

    Code manage general settings

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Setting.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Setting.java
@@ -25,6 +25,10 @@ public class Setting {
 		
 	}
 	
+	public Setting(String key) {
+		this.key = key;
+	}
+	
 	public Setting(String key, String value, SettingCategory category) {
 		this.key = key;
 		this.value = value;
@@ -53,6 +57,36 @@ public class Setting {
 
 	public void setCategory(SettingCategory category) {
 		this.category = category;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((key == null) ? 0 : key.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Setting other = (Setting) obj;
+		if (key == null) {
+			if (other.key != null)
+				return false;
+		} else if (!key.equals(other.key))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Setting [key=" + key + ", value=" + value + "]";
 	}
 	
 	
