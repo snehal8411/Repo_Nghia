201540471a3aae3733ac6fbf3d87e849f99e9d88
Nam Ha Minh <Nam Ha Minh@devpc>
2021-04-25 17:33:10 +0700

    Code controller layer for country

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Country.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Country.java
@@ -30,11 +30,22 @@ public class Country {
 		
 	}
 	
+	public Country(Integer id, String name, String code) {
+		this.id = id;
+		this.name = name;
+		this.code = code;
+	}
+
+
 	public Country(String name, String code) {
 		this.name = name;
 		this.code = code;
 	}
 
+	public Country(String name) {
+		this.name = name;
+	}
+	
 	public Integer getId() {
 		return id;
 	}
@@ -57,14 +68,6 @@ public class Country {
 
 	public void setCode(String code) {
 		this.code = code;
-	}
-
-	public Set<State> getStates() {
-		return states;
-	}
-
-	public void setStates(Set<State> states) {
-		this.states = states;
 	}
 
 	@Override
