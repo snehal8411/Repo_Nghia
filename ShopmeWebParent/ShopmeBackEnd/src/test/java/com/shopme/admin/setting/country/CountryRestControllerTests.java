93ae5683d27e47bd932b1e50857385d1a680e19e
Nam Ha Minh <Nam Ha Minh@devpc>
2021-04-29 06:15:52 +0700

    Code controller layer for state/province

--- ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/setting/country/CountryRestControllerTests.java
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/setting/country/CountryRestControllerTests.java
@@ -12,8 +12,6 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
