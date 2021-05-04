44d2a8e533fcd2407efc3889a8161101cffc0db8
Nam Ha Minh <Nam Ha Minh@devpc>
2021-05-05 06:46:37 +0700

    Code customer registration form

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/setting/StateRestController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/setting/StateRestController.java
@@ -1,14 +1,11 @@
-package com.shopme.admin.setting.state;
+package com.shopme.setting;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shopme.common.entity.Country;
@@ -20,7 +17,7 @@ public class StateRestController {
 
 	@Autowired private StateRepository repo;
 	
-	@GetMapping("/states/list_by_country/{id}")
+	@GetMapping("/settings/list_states_by_country/{id}")
 	public List<StateDTO> listByCountry(@PathVariable("id") Integer countryId) {
 		List<State> listStates = repo.findByCountryOrderByNameAsc(new Country(countryId));
 		List<StateDTO> result = new ArrayList<>();
@@ -31,15 +28,5 @@ public class StateRestController {
 		
 		return result;
 	}
-	
-	@PostMapping("/states/save")
-	public String save(@RequestBody State state) {
-		State savedState = repo.save(state);
-		return String.valueOf(savedState.getId());
-	}
-	
-	@DeleteMapping("/states/delete/{id}")
-	public void delete(@PathVariable("id") Integer id) {
-		repo.deleteById(id);
-	}
+
 }
