c50e0272cae66f8ca87fe9f9349630477581ae12
Nam Ha Minh <Nam Ha Minh@devpc>
2021-05-17 15:20:05 +0700

    Update authentication type of customer

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerService.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.shopme.common.entity.AuthenticationType;
 import com.shopme.common.entity.Country;
 import com.shopme.common.entity.Customer;
 import com.shopme.setting.CountryRepository;
@@ -57,6 +58,12 @@ public class CustomerService {
 		} else {
 			customerRepo.enable(customer.getId());
 			return true;
+		}
+	}
+	
+	public void updateAuthentication(Customer customer, AuthenticationType type) {
+		if (!customer.getAuthenticationType().equals(type)) {
+			customerRepo.updateAuthenticationType(customer.getId(), type);
 		}
 	}
 }
