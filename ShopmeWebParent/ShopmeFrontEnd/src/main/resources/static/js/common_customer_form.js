e4e81458dee26f5c1c0b77ff010068dc64a62f33
Nam Ha Minh <Nam Ha Minh@shopme.com>
2021-05-27 16:36:36 +0700

    Code quantity control component

--- ShopmeWebParent/ShopmeFrontEnd/src/main/resources/static/js/common_customer_form.js
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/resources/static/js/common_customer_form.js
@@ -37,17 +37,3 @@ function checkPasswordMatch(confirmPassword) {
 		confirmPassword.setCustomValidity("");
 	}
 }
-
-function showModalDialog(title, message) {
-	$("#modalTitle").text(title);
-	$("#modalBody").text(message);
-	$("#modalDialog").modal();
-}
-
-function showErrorModal(message) {
-	showModalDialog("Error", message);
-}
-
-function showWarningModal(message) {
-	showModalDialog("Warning", message);
-}	
