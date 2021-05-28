117d2bbcf0ea83989da5441b1c3ea9394f96dde6
Nam Ha Minh <Nam Ha Minh@devpc>
2021-05-28 09:30:47 +0700

    Code add product to shopping cart

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/Utility.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/Utility.java
@@ -5,7 +5,11 @@ import java.util.Properties;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.security.authentication.RememberMeAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 
+import com.shopme.security.oauth.CustomerOAuth2User;
 import com.shopme.setting.EmailSettingBag;
 
 public class Utility {
@@ -31,4 +35,22 @@ public class Utility {
 		
 		return mailSender;
 	}
+	
+	public static String getEmailOfAuthenticatedCustomer(HttpServletRequest request) {
+		Object principal = request.getUserPrincipal();
+		if (principal == null) return null;
+		
+		String customerEmail = null;
+		
+		if (principal instanceof UsernamePasswordAuthenticationToken 
+				|| principal instanceof RememberMeAuthenticationToken) {
+			customerEmail = request.getUserPrincipal().getName();
+		} else if (principal instanceof OAuth2AuthenticationToken) {
+			OAuth2AuthenticationToken oauth2Token = (OAuth2AuthenticationToken) principal;
+			CustomerOAuth2User oauth2User = (CustomerOAuth2User) oauth2Token.getPrincipal();
+			customerEmail = oauth2User.getEmail();
+		}
+		
+		return customerEmail;
+	}	
 }
