9716f7e96a62985c55dad77e402a21529ac2dc0f
Nam Ha Minh <Nam Ha Minh@devpc>
2021-05-30 17:41:01 +0700

    Code remove product from shopping cart

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/shoppingcart/ShoppingCartRestController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/shoppingcart/ShoppingCartRestController.java
@@ -3,6 +3,7 @@ package com.shopme.shoppingcart;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,5 +56,19 @@ public class ShoppingCartRestController {
 		} catch (CustomerNotFoundException ex) {
 			return "You must login to change quantity of product.";
 		}	
+	}
+	
+	@DeleteMapping("/cart/remove/{productId}")
+	public String removeProduct(@PathVariable("productId") Integer productId,
+			HttpServletRequest request) {
+		try {
+			Customer customer = getAuthenticatedCustomer(request);
+			cartService.removeProduct(productId, customer);
+			
+			return "The product has been removed from your shopping cart.";
+			
+		} catch (CustomerNotFoundException e) {
+			return "You must login to remove product.";
+		}
 	}
 }
