e1502826312396ed156ec07fa1ab97f81e365f16
Nam Ha Minh <Nam Ha Minh@shopme.com>
2021-05-31 07:29:35 +0700

    format currency in shopping cart

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/setting/SettingFilter.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/setting/SettingFilter.java
@@ -38,6 +38,7 @@ public class SettingFilter implements Filter {
 		
 		generalSettings.forEach(setting -> {
 			request.setAttribute(setting.getKey(), setting.getValue());
+			System.out.println(setting.getKey() + " == > " + setting.getValue());
 		});
 		
 		chain.doFilter(request, response);
