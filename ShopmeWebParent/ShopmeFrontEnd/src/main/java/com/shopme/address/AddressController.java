4c5602024e47b051dee088df6ff0d1da5561ef0f
Nam Ha Minh <Nam Ha Minh@DESKTOP-V712D93>
2021-06-24 07:49:42 +0700

    Update shopping cart page for checkout

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/address/AddressController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/address/AddressController.java
@@ -104,6 +104,13 @@ public class AddressController {
 		Customer customer = getAuthenticatedCustomer(request);
 		addressService.setDefaultAddress(addressId, customer.getId());
 		
-		return "redirect:/address_book"; 
+		String redirectOption = request.getParameter("redirect");
+		String redirectURL = "redirect:/address_book";
+		
+		if ("cart".equals(redirectOption)) {
+			redirectURL = "redirect:/cart";
+		}		
+		
+		return redirectURL; 
 	}
 }
--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/shoppingcart/ShoppingCartController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/shoppingcart/ShoppingCartController.java
@@ -10,14 +10,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.shopme.Utility;
+import com.shopme.address.AddressService;
+import com.shopme.common.entity.Address;
 import com.shopme.common.entity.CartItem;
 import com.shopme.common.entity.Customer;
+import com.shopme.common.entity.ShippingRate;
 import com.shopme.customer.CustomerService;
+import com.shopme.shipping.ShippingRateService;
 
 @Controller
 public class ShoppingCartController {
 	@Autowired private CustomerService customerService;
 	@Autowired private ShoppingCartService cartService;
+	@Autowired private AddressService addressService;
+	@Autowired private ShippingRateService shipService;
 	
 	@GetMapping("/cart")
 	public String viewCart(Model model, HttpServletRequest request) {
@@ -30,6 +36,19 @@ public class ShoppingCartController {
 			estimatedTotal += item.getSubtotal();
 		}
 		
+		Address defaultAddress = addressService.getDefaultAddress(customer);
+		ShippingRate shippingRate = null;
+		boolean usePrimaryAddressAsDefault = false;
+		
+		if (defaultAddress != null) {
+			shippingRate = shipService.getShippingRateForAddress(defaultAddress);
+		} else {
+			usePrimaryAddressAsDefault = true;
+			shippingRate = shipService.getShippingRateForCustomer(customer);
+		}
+		
+		model.addAttribute("usePrimaryAddressAsDefault", usePrimaryAddressAsDefault);
+		model.addAttribute("shippingSupported", shippingRate != null);
 		model.addAttribute("cartItems", cartItems);
 		model.addAttribute("estimatedTotal", estimatedTotal);
 		
