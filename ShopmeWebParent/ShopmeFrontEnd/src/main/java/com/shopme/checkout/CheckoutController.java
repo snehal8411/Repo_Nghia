8dc27d24056294dc08512193d92f5808a0c89a90
Nam Ha Minh <Nam Ha Minh@DESKTOP-V712D93>
2021-06-27 22:25:58 +0700

    Code place order with COD

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/checkout/CheckoutController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/checkout/CheckoutController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.shopme.Utility;
 import com.shopme.address.AddressService;
@@ -15,7 +16,9 @@ import com.shopme.common.entity.Address;
 import com.shopme.common.entity.CartItem;
 import com.shopme.common.entity.Customer;
 import com.shopme.common.entity.ShippingRate;
+import com.shopme.common.entity.order.PaymentMethod;
 import com.shopme.customer.CustomerService;
+import com.shopme.order.OrderService;
 import com.shopme.shipping.ShippingRateService;
 import com.shopme.shoppingcart.ShoppingCartService;
 
@@ -27,6 +30,7 @@ public class CheckoutController {
 	@Autowired private AddressService addressService;
 	@Autowired private ShippingRateService shipService;
 	@Autowired private ShoppingCartService cartService;
+	@Autowired private OrderService orderService;
 	
 	@GetMapping("/checkout")
 	public String showCheckoutPage(Model model, HttpServletRequest request) {
@@ -60,4 +64,29 @@ public class CheckoutController {
 		String email = Utility.getEmailOfAuthenticatedCustomer(request);				
 		return customerService.getCustomerByEmail(email);
 	}	
+	
+	@PostMapping("/place_order")
+	public String placeOrder(HttpServletRequest request) {
+		String paymentType = request.getParameter("paymentMethod");
+		PaymentMethod paymentMethod = PaymentMethod.valueOf(paymentType);
+		
+		Customer customer = getAuthenticatedCustomer(request);
+		
+		Address defaultAddress = addressService.getDefaultAddress(customer);
+		ShippingRate shippingRate = null;
+		
+		if (defaultAddress != null) {
+			shippingRate = shipService.getShippingRateForAddress(defaultAddress);
+		} else {
+			shippingRate = shipService.getShippingRateForCustomer(customer);
+		}
+				
+		List<CartItem> cartItems = cartService.listCartItems(customer);
+		CheckoutInfo checkoutInfo = checkoutService.prepareCheckout(cartItems, shippingRate);
+		
+		orderService.createOrder(customer, defaultAddress, cartItems, paymentMethod, checkoutInfo);
+		cartService.deleteByCustomer(customer);
+		
+		return "checkout/order_completed";
+	}
 }
