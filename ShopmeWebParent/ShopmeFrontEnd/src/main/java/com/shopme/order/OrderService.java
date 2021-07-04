63175acf07cf04ec6c152acf6eb7e2932aeaa762
Nam Ha Minh <Nam Ha Minh@DESKTOP-V712D93>
2021-07-04 14:04:24 +0700

    Code validate PayPal order

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/order/OrderService.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/order/OrderService.java
@@ -26,7 +26,13 @@ public class OrderService {
 			PaymentMethod paymentMethod, CheckoutInfo checkoutInfo) {
 		Order newOrder = new Order();
 		newOrder.setOrderTime(new Date());
-		newOrder.setStatus(OrderStatus.NEW);
+		
+		if (paymentMethod.equals(PaymentMethod.PAYPAL)) {
+			newOrder.setStatus(OrderStatus.PAID);
+		} else {
+			newOrder.setStatus(OrderStatus.NEW);
+		}
+		
 		newOrder.setCustomer(customer);
 		newOrder.setProductCost(checkoutInfo.getProductCost());
 		newOrder.setSubtotal(checkoutInfo.getProductTotal());
