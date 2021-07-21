30720e0b744777436ed0cbc1edccb91a259e943e
Nam Ha Minh <Nam Ha Minh@DESKTOP-V712D93>
2021-07-21 16:24:16 +0700

    Calculate shipping cost of a product

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/shippingrate/ShippingRateService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/shippingrate/ShippingRateService.java
@@ -9,17 +9,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.shopme.admin.paging.PagingAndSortingHelper;
+import com.shopme.admin.product.ProductRepository;
 import com.shopme.admin.setting.country.CountryRepository;
 import com.shopme.common.entity.Country;
 import com.shopme.common.entity.ShippingRate;
+import com.shopme.common.entity.product.Product;
 
 @Service
 @Transactional
 public class ShippingRateService {
 	public static final int RATES_PER_PAGE = 10;
-	
+	private static final int DIM_DIVISOR = 139;	
 	@Autowired private ShippingRateRepository shipRepo;
 	@Autowired private CountryRepository countryRepo;
+	@Autowired private ProductRepository productRepo;
 	
 	public void listByPage(int pageNum, PagingAndSortingHelper helper) {
 		helper.listEntities(pageNum, RATES_PER_PAGE, shipRepo);
@@ -66,5 +69,22 @@ public class ShippingRateService {
 			
 		}
 		shipRepo.deleteById(id);
-	}	
+	}
+	
+	public float calculateShippingCost(Integer productId, Integer countryId, String state) 
+			throws ShippingRateNotFoundException {
+		ShippingRate shippingRate = shipRepo.findByCountryAndState(countryId, state);
+		
+		if (shippingRate == null) {
+			throw new ShippingRateNotFoundException("No shipping rate found for the given "
+					+ "destination. You have to enter shipping cost manually.");
+		}
+		
+		Product product = productRepo.findById(productId).get();
+		
+		float dimWeight = (product.getLength() * product.getWidth() * product.getHeight()) / DIM_DIVISOR;
+		float finalWeight = product.getWeight() > dimWeight ? product.getWeight() : dimWeight;
+				
+		return finalWeight * shippingRate.getRate();
+	}
 }
