6c049b2f264eff7de534737bea7050f1d07eb251
Nam Ha Minh <Nam Ha Minh@DESKTOP-V712D93>
2021-07-23 11:20:18 +0700

    Code add products to an order (part 1)

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -69,7 +69,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 					.key("AbcDefgHijKlmnOpqrs_1234567890")
 					.tokenValiditySeconds(7 * 24 * 60 * 60);
 					;
-			
+			http.headers().frameOptions().sameOrigin();
 	}
 
 	@Override
