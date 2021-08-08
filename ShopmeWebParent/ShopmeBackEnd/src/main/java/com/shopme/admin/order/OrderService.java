98ef8d6460032cd058240ca81e83314cdfb083d1
Nam Ha Minh <Nam Ha Minh@DESKTOP-V712D93>
2021-08-08 15:33:53 +0700

    Code service and controller for updating order status

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/order/OrderService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/order/OrderService.java
@@ -1,5 +1,6 @@
 package com.shopme.admin.order;
 
+import java.util.Date;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -14,6 +15,8 @@ import com.shopme.admin.paging.PagingAndSortingHelper;
 import com.shopme.admin.setting.country.CountryRepository;
 import com.shopme.common.entity.Country;
 import com.shopme.common.entity.order.Order;
+import com.shopme.common.entity.order.OrderStatus;
+import com.shopme.common.entity.order.OrderTrack;
 
 @Service
 public class OrderService {
@@ -77,4 +80,26 @@ public class OrderService {
 		
 		orderRepo.save(orderInForm);
 	}	
+	
+	public void updateStatus(Integer orderId, String status) {
+		Order orderInDB = orderRepo.findById(orderId).get();
+		OrderStatus statusToUpdate = OrderStatus.valueOf(status);
+		
+		if (!orderInDB.hasStatus(statusToUpdate)) {
+			List<OrderTrack> orderTracks = orderInDB.getOrderTracks();
+			
+			OrderTrack track = new OrderTrack();
+			track.setOrder(orderInDB);
+			track.setStatus(statusToUpdate);
+			track.setUpdatedTime(new Date());
+			track.setNotes(statusToUpdate.defaultDescription());
+			
+			orderTracks.add(track);
+			
+			orderInDB.setStatus(statusToUpdate);
+			
+			orderRepo.save(orderInDB);
+		}
+		
+	}
 }
