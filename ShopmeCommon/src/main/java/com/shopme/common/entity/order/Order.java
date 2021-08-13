55fe538d7625fb0b5444a56045373f930a07369d
Nam Ha Minh <Nam Ha Minh@DESKTOP-V712D93>
2021-08-13 22:42:06 +0700

    Code orders listing for customer

--- ShopmeCommon/src/main/java/com/shopme/common/entity/order/Order.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/order/Order.java
@@ -292,6 +292,11 @@ public class Order extends AbstractAddress {
 	public boolean isDelivered() {
 		return hasStatus(OrderStatus.DELIVERED);
 	}
+
+	@Transient
+	public boolean isReturnRequested() {
+		return hasStatus(OrderStatus.RETURN_REQUESTED);
+	}	
 	
 	@Transient
 	public boolean isReturned() {
@@ -307,4 +312,19 @@ public class Order extends AbstractAddress {
 		
 		return false;
 	}
+	
+	@Transient
+	public String getProductNames() {
+		String productNames = "";
+		
+		productNames = "<ul>";
+		
+		for (OrderDetail detail : orderDetails) {
+			productNames += "<li>" + detail.getProduct().getShortName() + "</li>";			
+		}
+		
+		productNames += "</ul>";
+		
+		return productNames;
+	}	
 }
