fa1fa7a4f1e27062f5e9fe85c3ab511014327fe3
namhaminh <hainatu@gmail.com>
2021-11-12 08:27:46 +0700

    Code review listing of a product

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/review/ReviewController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/review/ReviewController.java
@@ -15,8 +15,11 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.shopme.Utility;
 import com.shopme.common.entity.Customer;
 import com.shopme.common.entity.Review;
+import com.shopme.common.entity.product.Product;
+import com.shopme.common.exception.ProductNotFoundException;
 import com.shopme.common.exception.ReviewNotFoundException;
 import com.shopme.customer.CustomerService;
+import com.shopme.product.ProductService;
 
 @Controller
 public class ReviewController {
@@ -24,6 +27,7 @@ public class ReviewController {
 	
 	@Autowired private ReviewService reviewService;
 	@Autowired private CustomerService customerService;
+	@Autowired private ProductService productService;
 	
 	@GetMapping("/reviews")
 	public String listFirstPage(Model model) {
@@ -80,5 +84,51 @@ public class ReviewController {
 			ra.addFlashAttribute("message", ex.getMessage());
 			return defaultRedirectURL;		
 		}
+	}
+	
+	@GetMapping("/ratings/{productAlias}/page/{pageNum}") 
+	public String listByProductByPage(Model model,
+				@PathVariable(name = "productAlias") String productAlias,
+				@PathVariable(name = "pageNum") int pageNum,
+				String sortField, String sortDir) {
+		
+		Product product = null;
+		
+		try {
+			product = productService.getProduct(productAlias);
+		} catch (ProductNotFoundException ex) {
+			return "error/404";
+		}
+		
+		Page<Review> page = reviewService.listByProduct(product, pageNum, sortField, sortDir);
+		List<Review> listReviews = page.getContent();
+		
+		model.addAttribute("totalPages", page.getTotalPages());
+		model.addAttribute("totalItems", page.getTotalElements());
+		model.addAttribute("currentPage", pageNum);
+		model.addAttribute("sortField", sortField);
+		model.addAttribute("sortDir", sortDir);
+		model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
+		
+		model.addAttribute("listReviews", listReviews);
+		model.addAttribute("product", product);
+
+		long startCount = (pageNum - 1) * ReviewService.REVIEWS_PER_PAGE + 1;
+		model.addAttribute("startCount", startCount);
+		
+		long endCount = startCount + ReviewService.REVIEWS_PER_PAGE - 1;
+		if (endCount > page.getTotalElements()) {
+			endCount = page.getTotalElements();
+		}
+		
+		model.addAttribute("endCount", endCount);
+		model.addAttribute("pageTitle", "Reviews for " + product.getShortName());
+		
+		return "reviews/reviews_product";
+	}
+	
+	@GetMapping("/ratings/{productAlias}")
+	public String listByProductFirstPage(@PathVariable(name = "productAlias") String productAlias, Model model) {
+		return listByProductByPage(model, productAlias, 1, "reviewTime", "desc");
 	}	
 }
--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/review/ReviewService.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/review/ReviewService.java
@@ -45,5 +45,14 @@ public class ReviewService {
 		Pageable pageable = PageRequest.of(0, 3, sort);
 		
 		return repo.findByProduct(product, pageable);		
-	}	
+	}
+	
+	public Page<Review> listByProduct(Product product, int pageNum, String sortField, String sortDir) {
+		Sort sort = Sort.by(sortField);
+		sort = sortDir.equals("asc") ? sort.ascending() : sort.descending(); 
+		Pageable pageable = PageRequest.of(pageNum - 1, REVIEWS_PER_PAGE, sort);
+		
+		return repo.findByProduct(product, pageable);
+	}
+	
 }
