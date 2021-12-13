ea5423560c2cc6289d4bea8a1df27137f572036d
namhaminh <hainatu@gmail.com>
2021-12-13 23:19:35 +0700

    Code controller layer for review voting module

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/review/ReviewRepository.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/review/ReviewRepository.java
@@ -28,8 +28,8 @@ public interface ReviewRepository extends JpaRepository<Review, Integer> {
 			+ "r.product.id = ?2")
 	public Long countByCustomerAndProduct(Integer customerId, Integer productId);
 	
-	@Query("UPDATE Review r SET r.votes = (SELECT SUM(v.votes) FROM ReviewVote v"
-			+ " WHERE v.review.id=?1) WHERE r.id = ?1")
+	@Query("UPDATE Review r SET r.votes = COALESCE((SELECT SUM(v.votes) FROM ReviewVote v"
+			+ " WHERE v.review.id=?1), 0) WHERE r.id = ?1")
 	@Modifying
 	public void updateVoteCount(Integer reviewId);
 	
--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/review/vote/ReviewVoteService.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/review/vote/ReviewVoteService.java
@@ -2,6 +2,8 @@ package com.shopme.review.vote;
 
 import java.util.NoSuchElementException;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,7 @@ import com.shopme.common.entity.ReviewVote;
 import com.shopme.review.ReviewRepository;
 
 @Service
+@Transactional
 public class ReviewVoteService {
 	
 	@Autowired private ReviewRepository reviewRepo;
